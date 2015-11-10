e529f8f1b0f78b50d6612d82f9ee078d9bd57558
janeluck <janeluck158@gmail.com>
2015-11-10 16:37:57 +0800

    update

--- toMaxInt.js
+++ toMaxInt.js
@@ -16,7 +16,7 @@ module.exports = function(n) {
     } else {
         if (n == 10) {
             // n = 10
-            return 10;
+            result = 10;
         } else {
             // n > 10
             result = str.slice(1) -  Math.pow(10, secondDigit-1)*5 > 0 ? (str.substr(0, 1) * 1 + 1) * Math.pow(10, secondDigit) : str.substr(0, 1) * Math.pow(10, secondDigit) +Math.pow(10, secondDigit-1)*5;
