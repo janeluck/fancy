1800e1b37843ab1d1c1354ff9c6bbcc99942e5e6
janeluck <janeluck158@gmail.com>
2015-11-10 16:21:50 +0800

    toMaxInt

--- test.js
+++ test.js
@@ -1,3 +1,30 @@
 /**
  * Created by janeluck on 11/10/15.
  */
+
+var should = require('should'),
+    toMaxInt = require('./toMaxInt');
+
+describe('Convert numbers to max int', function(){
+    it('n < 10', function() {
+
+        toMaxInt(1).should.eql(5);
+        toMaxInt(5).should.eql(5);
+        toMaxInt(6).should.eql(10);
+    });
+
+
+    it('n = 10', function() {
+
+        toMaxInt(10).should.eql(10);
+    });
+
+
+    it('n > 10', function() {
+
+        toMaxInt(120).should.eql(150);
+        toMaxInt(1001).should.eql(1500);
+        toMaxInt(1501).should.eql(2000);
+        toMaxInt(3700).should.eql(4000);
+    })
+})
--- toMaxInt.js
+++ toMaxInt.js
@@ -1,3 +1,28 @@
 /**
  * Created by janeluck on 11/10/15.
  */
+
+// 目前只考虑正整数型
+// @param n {Number}
+
+module.exports = function(n) {
+    var result = 0, str,secondDigit;
+    str = n.toString();
+    secondDigit = str.length - 1;
+    if (!secondDigit){
+        // n < 10
+        result = (n <= 5 ? 5 : 10);
+
+    } else {
+        if (n == 10) {
+            // n = 10
+            return 10;
+        } else {
+            // n > 10
+            result = str.slice(1) -  Math.pow(10, secondDigit-1)*5 > 0 ? (str.substr(0, 1) * 1 + 1) * Math.pow(10, secondDigit) : str.substr(0, 1) * Math.pow(10, secondDigit) +Math.pow(10, secondDigit-1)*5;
+        }
+
+    }
+
+    return result;
+}
